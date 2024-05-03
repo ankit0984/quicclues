32cf9732f2ce4e6c7e210437000f56508ad4bd10
ankit <ak1410mk@gmail.com>
2024-05-03 09:45:44 +0530

    Co-authored-by: Akshay kumar Pandit <Akshaypandit33@users.noreply.github.com>

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-
+import { logo1 } from '../assets'
 function Footer() {
   return (
     <footer className= "text-white body-font">
   <div className= "container px-5 py-20 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
     <div className= "w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
       <a className= "flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap ="round" strokeLinejoin ="round" strokeWidth ="2" className= "w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-          <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-        </svg>
+        
+        <img src={logo1} alt="" srcset="" className= "w-14 h-14 text-white p-2  rounded-full"/>
+
         <span className= "text-gray-500 ml-3 text-xl">Quicclues</span>
       </a>
       <p className= "mt-2 text-sm text-gray-500">Empowering your digital dreams: Quicclues-where visionaries thrives!</p>
